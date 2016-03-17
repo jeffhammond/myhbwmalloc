27eb15b0ba94ea0f3758175d42d3bab5d97fe4d0
Jeff Hammond <jeff.science@gmail.com>
2016-03-17 15:06:12 -0700

    perf diff

--- test/test_performance.c
+++ test/test_performance.c
@@ -16,22 +16,42 @@ int main(int argc, char * argv[])
     printf("%s returned %s\n", "hbw_check_available", avail==0 ? "SUCCESS" : "FAILURE");
     assert(avail == 0);
 
-    char * a = hbw_calloc(bytes,1);
-    assert(a != NULL);
+    {
+        char * a = calloc(bytes,1);
+        assert(a != NULL);
 
-    char * b = hbw_calloc(bytes,1);
-    assert(b != NULL);
+        char * b = calloc(bytes,1);
+        assert(b != NULL);
 
-    double t0 = MPI_Wtime();
-    for (size_t i=0; i<bytes; i++) {
-        b[i] = a[i];
+        double t0 = MPI_Wtime();
+        for (size_t i=0; i<bytes; i++) {
+            b[i] = a[i];
+        }
+        double t1 = MPI_Wtime();
+        double dt = t1-t0;
+        printf("default heap: dt = %e, bw = %e B/s\n", dt, bytes/dt);
+
+        free(b);
+        free(a);
     }
-    double t1 = MPI_Wtime();
-    double dt = t1-t0;
-    printf("dt = %e, bw = %e B/s\n", dt, bytes/dt);
+    {
+        char * a = hbw_calloc(bytes,1);
+        assert(a != NULL);
 
-    hbw_free(b);
-    hbw_free(a);
+        char * b = hbw_calloc(bytes,1);
+        assert(b != NULL);
+
+        double t0 = MPI_Wtime();
+        for (size_t i=0; i<bytes; i++) {
+            b[i] = a[i];
+        }
+        double t1 = MPI_Wtime();
+        double dt = t1-t0;
+        printf("hbw heap: dt = %e, bw = %e B/s\n", dt, bytes/dt);
+
+        hbw_free(b);
+        hbw_free(a);
+    }
 
     MPI_Finalize();
 
